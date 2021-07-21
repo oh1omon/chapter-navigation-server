818c8a2cfe5506894849d3f1ed2f377f35af3ff5
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-21 19:36:37 +0300

    docs: added comments to the code and README.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Chapter Navigation Server
+
+---
+
+Important: It is not an API, just a cors-anywhere server
+
+It is needed due to the cors policy of the source file needed on FE host.
+
+---
+
+To start:
+
+-   npm start
+
+To use:
+
+-   Just append your link right after the end of the url of this server, like: http://127.0.0.1:8080/<https://your-link-here.com>
+
+---
+
+I am using Git Flow for organizing branches and VCS, and Standard Version for making changes to CHANGELOG.md and versions
--- index.js
+++ index.js
@@ -4,14 +4,18 @@ require('dotenv').config()
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || '127.0.0.1'
 
+// Here we trying to create a cors-anywhere-like server
 try {
 	cors_proxy
 		.createServer({
+			// Server will remove any cookie headers
 			removeHeaders: ['cookie', 'cookie2'],
 		})
+		// Listening on HOST:PORT from .env file, or from variable replacers
 		.listen(PORT, HOST, () => {
 			;`Server is working on ${HOST}:${PORT}`
 		})
 } catch (e) {
+	// Caught error will be printed to us
 	console.log(e)
 }
