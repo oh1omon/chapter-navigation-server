0758e2e8af14860fb3edf941f12b9751dd6960bf
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-30 21:22:33 +0300

    feat: deleted Host variable

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const cors_proxy = require('cors-anywhere')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8080
-const HOST = process.env.HOST || '127.0.0.1'
 
 // Here we trying to create a cors-anywhere-like server
 try {
@@ -12,8 +11,8 @@ try {
 			removeHeaders: ['cookie', 'cookie2'],
 		})
 		// Listening on HOST:PORT from .env file, or from variable replacers
-		.listen(PORT, HOST, () => {
-			;`Server is working on ${HOST}:${PORT}`
+		.listen(PORT, () => {
+			;`Server is working on localhost:${PORT}`
 		})
 } catch (e) {
 	// Caught error will be printed to us
